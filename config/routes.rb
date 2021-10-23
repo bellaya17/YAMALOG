601afcd884543068a8668b5497f4f18c5e68074f
bellaya17 <belltreeayaka@gmail.com>
2021-10-23 07:04:30 +0000

    [Add]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get"about" => "top#about", as:"about"
   devise_for :users
-  resources :post_images
+  resources :post_images, only: [:new, :create, :index, :show, :destroy, :edit] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 
 end
